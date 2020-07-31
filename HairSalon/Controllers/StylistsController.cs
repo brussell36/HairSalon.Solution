e6b591785578519b2019b650c0581c8bc45a1f28
Ben Russell <benjaminrussell36@gmail.com>
2020-07-31 09:22:01 -0700

    Add route to StylistsController and corresponding Index.cshtml view.

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace HairSalon.Controllers
+{
+  public class StylistsController : Controllers
+  {
+    private readonly HairSalonContext _db;
+    public StylistsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model);
+    }
+  }
+}
